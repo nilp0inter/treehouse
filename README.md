cebf49f4db906c88ed9e61ffc97589860818e5d7
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-02-04 12:35:54 +0100

    chore: working nix development for yew thanks to https://github.com/yewstack/yew-trunk-minimal-template/pull/5

---
+++ README.md
@@ -0,0 +1,24 @@
+# Treehouse
+
+## Running
+
+```bash
+nix develop
+trunk serve
+```
+
+Rebuilds the app whenever a change is detected and runs a local server to host it.
+
+There's also the `trunk watch` command which does the same thing but without hosting it.
+
+### Release
+
+```bash
+trunk build --release
+```
+
+This builds the app in release mode similar to `cargo build --release`.
+You can also pass the `--release` flag to `trunk serve` if you need to get every last drop of performance.
+
+Unless overwritten, the output will be located in the `dist` directory.
+
--- flake.nix
+++ flake.nix
@@ -1,24 +1,70 @@
 {
-  description = "Minimal rust wasm32-unknown-unknown example";
-
   inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "nixpkgs/nixos-22.11";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, fenix }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; }; in
-      {
-        defaultPackage = pkgs.rustPlatform.buildRustPackage {
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        fenix-system = fenix.packages.x86_64-linux;
+        rust-toolchain = (with fenix-system;
+          combine [
+            default.toolchain
+            complete.rust-src
+            targets.wasm32-unknown-unknown.latest.rust-std
+          ]);
+        rustPlatform =
+          # pkgs.makeRustPlatform { inherit (fenix-system.minimal) cargo rustc; };
+          pkgs.makeRustPlatform {
+            rustc = rust-toolchain;
+            cargo = rust-toolchain;
+          };
+      in {
+        defaultPackage = rustPlatform.buildRustPackage {
           pname = "treehouse";
           version = "0.1.0";
           src = ./.;
 
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
+          nativeBuildInputs = with pkgs; [
+            trunk
+            wasm-bindgen-cli
+            nodePackages.sass
+            pkgconfig
+          ];
+
+          cargoLock = { lockFile = ./Cargo.lock; };
+
+          buildPhase = "trunk build";
+          installPhase = "cp -r dist $out";
+
+          # TODO: Use $TMP instead of /build
+          XDG_CACHE_HOME = "/build/cache";
         };
-      }
-    );
+        devShell = pkgs.mkShell {
+          name = "rust-env";
+          src = ./.;
+
+          nativeBuildInputs = (with pkgs; [
+            rust-toolchain
+            fenix-system.rust-analyzer
+            wasm-bindgen-cli
+            nodePackages.sass
+            trunk
+            zlib
+            pkgconfig
+          ]);
+
+          shellHook = ''
+            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
+              pkgs.lib.makeLibraryPath [ pkgs.zlib ]
+            }"'';
+        };
+      });
 }
+
